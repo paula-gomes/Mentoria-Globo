3824e2233a9a5f4f405cd289844f3a494b20d7ef
Paula Gomes <gomes.pcristina@gmail.com>
2020-09-29 19:07:22 -0300

    Criacao DAO para acesso no bd

---
+++ src/DAO/inserir_votos.js
@@ -0,0 +1,47 @@
+class VotosDao {
+
+    constructor(bd) {
+        this._bd = bd;
+    }
+
+    adicionaParticipante(nome, url) {
+
+        return new Promise((resolve, reject) => {
+
+            this._bd.run(
+                `INSERT INTO participantes (nome,url) VALUES (?,?)`,
+                [nome, url],
+                (err) => {
+                    if (err) {
+                        console.log(err)
+                        return reject('Não foi possivel add participante')
+                    }
+                    return resolve();
+
+                }
+            );
+
+        });
+    }
+
+    adicionaVoto(id_participante){
+
+        return new Promise((resolve, reject) => {
+
+            this._bd.run(
+                `INSERT INTO votos (id_participante) VALUE (?)`,
+                [id_participante],
+                (err) => {
+                    if (err) {
+                        console.log(err)
+                        return reject('Não foi possivel add voto')
+                    }
+                    return resolve();
+
+                }
+            );
+
+        });
+
+    };
+}
